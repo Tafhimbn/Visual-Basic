32191827ad7eb7f0e6a166c2c35e6c6f4908ea3b
Tafhimbn <tafhim.aiub@gmail.com>
2025-10-05 21:00:37 +0600

    Cleaned up and ignored .vs folder

--- 1_variable/Form1.Designer.vb
+++ 1_variable/Form1.Designer.vb
@@ -22,10 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        btn_check_variable = New Button()
+        SuspendLayout()
+        ' 
+        ' btn_check_variable
+        ' 
+        btn_check_variable.Location = New Point(334, 164)
+        btn_check_variable.Name = "btn_check_variable"
+        btn_check_variable.Size = New Size(137, 45)
+        btn_check_variable.TabIndex = 0
+        btn_check_variable.Text = "Check Variable"
+        btn_check_variable.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btn_check_variable)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents btn_check_variable As Button
 
 End Class
--- 1_variable/Form1.vb
+++ 1_variable/Form1.vb
@@ -1,3 +1,20 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_check_variable.Click
 
+        ' Variable Declaration in VB.NET
+        ' Syntax:
+        '         Dim variable_name As DataType
+        ' or
+        '        Dim variable_name As DataType = value
+
+        Dim frirst_name As String
+        frirst_name = "Tafhim"
+        Dim last_name As String = "Bin Nasir"
+        Dim age As Integer = 28
+        Dim height As Double = 5.8
+        Dim isStudent As Boolean = False
+        Dim joiningDate As Date = #10/5/2025#
+        MsgBox("My name is " & frirst_name & " " & last_name)
+
+    End Sub
 End Class
